f1e216c012dc87a2184c2280ee005b2fcab99fe9
holyshared <holy.shared.design@gmail.com>
2014-09-20 20:53:11 +0900

    add configs

---
+++ config/_env.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file is part of CoverallsKit.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+if (isset($_ENV['AURA_CONFIG_MODE']) === false) {
+    $_ENV['AURA_CONFIG_MODE'] = 'dev';
+}
