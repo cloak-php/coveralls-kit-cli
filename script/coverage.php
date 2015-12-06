4bc0b3069d47201a6031ce4116eb99d1042aaa4f
holyshared <holy.shared.design@gmail.com>
2015-12-06 15:34:04 +0900

    Update coverage script

--- script/coverage.php
+++ script/coverage.php
@@ -5,36 +5,16 @@ namespace coverallskit\example;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
-use cloak\Analyzer;
-use cloak\ConfigurationBuilder;
-use cloak\Result\File;
-use cloak\reporter\CompositeReporter;
-use cloak\reporter\LcovReporter;
-use cloak\reporter\ProcessingTimeReporter;
-use cloak\reporter\TextReporter;
+use cloak\CoverageAnalyzer;
+use cloak\configuration\ConfigurationLoader;
 
+$loader = new ConfigurationLoader();
+$configuration = $loader->loadConfiguration('cloak.toml');
 
-$analyzer = Analyzer::factory(function(ConfigurationBuilder $builder) {
-
-    $reporter = new CompositeReporter([
-        new LcovReporter(__DIR__ . '/report.lcov'),
-        new TextReporter(),
-        new ProcessingTimeReporter()
-    ]);
-
-    $builder->includeFile(function(File $file) {
-        return $file->matchPath('/src');
-    })->excludeFile(function(File $file) {
-        $matchExcludeTarget = $file->matchPath('/spec') || $file->matchPath('/vendor');
-        return $matchExcludeTarget;
-    })->reporter($reporter);
-
-});
-
+$analyzer = new CoverageAnalyzer($configuration);
 $analyzer->start();
 
-$argv = ['../vendor/bin/pho', '--stop'];
+$argv = [ '../vendor/bin/pho', '--stop' ];
 require_once __DIR__ . "/../vendor/bin/pho";
 
 $analyzer->stop();
-
